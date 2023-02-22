d75bcc564fba4ff06cbe27ae135ac3217ad666b0
tasnimbenslama <93452582+tasnimbenslama@users.noreply.github.com>
2023-02-22 20:43:17 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OffresEmploiComponentComponent,
+    HeaderComponent,
+    ArticlesComponentComponent,
+    TemplateDrivenFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
